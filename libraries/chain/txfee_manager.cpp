b47ed6a839e7a2c094edb7a60e3f32c98b4b0d8f
范杨 <fanyang.coder@gmail.com>
2018-12-20 14:50:06 +0800

    del spec fee for early eosforce account

--- libraries/chain/txfee_manager.cpp
+++ libraries/chain/txfee_manager.cpp
@@ -56,41 +56,6 @@ namespace eosio { namespace chain {
       return true;
    }
 
-   /*
-    * About Fee
-    *
-    * fee come from three mode:
-    *  - native, set in cpp
-    *  - set by eosio
-    *  - set by user
-    *
-    * and res limit can set a value or zero,
-    * all of this will make diff mode to calc res limit,
-    * support 1.0 EOS is for `C` cpu, `N` net and `R` ram,
-    * and cost fee by `f` EOS and extfee by `F` EOS
-    *
-    * then can give:
-    *  - native and no setfee : use native fee and unlimit res use
-    *  - eosio set fee {f, (c,n,r)}
-    *      (cpu_limit, net_limit, ram_limit) == (c + F*C, n + F*N, r + F*R)
-    *  - eosio set fee {f, (0,0,0)}
-    *      (cpu_limit, net_limit, ram_limit) == ((f+F)*C, (f+F)*N, (f+F)*R)
-    *  - user set fee {f, (0,0,0)}, user cannot set fee by c>0||n>0||r>0
-    *      (cpu_limit, net_limit, ram_limit) == ((f+F)*C, (f+F)*N, (f+F)*R)
-    *
-    *  so it can be check by:
-    *  if no setfee
-    *       if no native -> err
-    *       if native -> use native and unlimit res use
-    *  else
-    *       if res limit is (0,0,0) -> limit res by ((f+F)*C, (f+F)*N, (f+F)*R)
-    *       if res limit is (c,n,r) -> (c + F*C, n + F*N, r + F*R)
-    *
-    *  at the same time, eosio can set res limit > (0,0,0) and user cannot
-    *
-    */
-
-
    asset txfee_manager::get_required_fee( const controller& ctl, const transaction& trx)const
    {
       auto fee = asset(0);
@@ -105,30 +70,6 @@ namespace eosio { namespace chain {
    asset txfee_manager::get_required_fee( const controller& ctl, const action& act)const{
       const auto &db = ctl.db();
       const auto block_num = ctl.head_block_num();
-
-      // keep consensus for main net, some action in main net exec action
-      // like newaccount in diff account
-      {
-         if ((act.name == N(newaccount)) &&
-             ((act.account == N(eosio.bios))
-              || (act.account == N(eosio.token))
-             )) {
-            const auto native_fee = get_native_fee(block_num, config::system_account_name, act.name);
-            if (native_fee != asset(0)) {
-               return native_fee;
-            }
-         }
-
-         if ((act.name == N(transfer)) &&
-             (   (act.account == N(victor))
-                 || (act.account == N(eosvictor))
-             )) {
-            const auto native_fee = get_native_fee(block_num, config::system_account_name, act.name);
-            if (native_fee != asset(0)) {
-               return native_fee;
-            }
-         }
-      }
 
       // first check if changed fee
       try{
