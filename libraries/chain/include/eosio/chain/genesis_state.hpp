1d3f7b7fd2f4fa63fcd4049f53173fd0a507deec
范杨 <fanyang.coder@gmail.com>
2018-12-27 14:30:31 +0800

    use cfg from makefile to set system account pub key

--- libraries/chain/include/eosio/chain/genesis_state.hpp
+++ libraries/chain/include/eosio/chain/genesis_state.hpp
@@ -32,7 +32,7 @@ struct producer_tuple {
 struct genesis_state {
    genesis_state();
 
-   static const string eosio_root_key;
+   static const string system_account_root_key;
 
    chain_config   initial_configuration = {
       .max_block_net_usage                  = config::default_max_block_net_usage,
