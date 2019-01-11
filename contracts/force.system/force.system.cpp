bc40022937c7ce11b1a39c736189ed1a1c3cb6f3
bookcool <sunshuhan666@163.com>
2019-01-11 16:02:37 +0800

    newaccount contract set resource limits to 0 on delegate model

--- contracts/force.system/force.system.cpp
+++ contracts/force.system/force.system.cpp
@@ -16,4 +16,17 @@ namespace eosiosystem {
       eosio_assert( 3 <= params.max_authority_depth, "max_authority_depth should be at least 3" );
       set_blockchain_parameters( params );
    }
+
+    void system_contract::newaccount(account_name creator,account_name name,authority owner,authority active) {
+#if CONTRACT_RESOURCE_MODEL == RESOURCE_MODEL_DELEGATE
+
+       user_resources_table  userres( _self, newact);
+
+       userres.emplace( newact, [&]( auto& res ) {
+           res.owner = newact;
+       });
+
+       set_resource_limits( newact, 0, 0, 0 );
+#endif
+    }
 }
