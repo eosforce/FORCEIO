da542c5a8e36f913ee90e234d930a1a8b9ee0b45
bookcool <sunshuhan666@163.com>
2019-01-11 16:42:38 +0800

    #if compile delegate model init resource to 0

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -710,6 +710,9 @@ struct controller_impl {
                memory_db::token_account{ account.asset });
          const authority auth(public_key);
          create_native_account(account.name, auth, auth, false);
+#if RESOURCE_MODEL == RESOURCE_MODEL_FEE
+         resource_limits.set_account_limits(account.name, 0, 0, 0);
+#endif
       }
    }
 
