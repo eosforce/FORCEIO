ae635468650f8360ee93a1e7a7e6c4e45e514412
xuyp1991 <xuyapeng1991@qq.com>
2019-01-22 17:49:01 +0800

    addmarket commit

--- contracts/trade.market/trade.market.cpp
+++ contracts/trade.market/trade.market.cpp
@@ -8,7 +8,51 @@
 namespace eosio {
    //新增一个交易对   
    void market_maker::addmarket(account_name market_maker,trade_type type,string coinbase_symbol,asset coinbase_amount,account_name coinbase_account,
-         string coinmarket_symbol,asset coinmarket_amount,account_name coinmarket_account) {
+         string coinmarket_symbol,asset coinmarket_amount,account_name coinmarket_account,double ratio) {
+         //需要三个账户的权限
+         require_auth(market_maker);
+         require_auth(coinbase_account);
+         require_auth(coinmarket_account);
+         //校验币是否可用
+         auto coinbase_sym = coinbase_amount.symbol;
+         eosio_assert( coinbase_sym.is_valid(), "invalid symbol name" );
+         eosio_assert( coinbase_amount.is_valid(), "invalid supply");
+         eosio_assert( coinbase_amount.amount > 0, "max-supply must be positive");
+         //校验币是否可用
+         auto coinmarket_sym = coinbase_amount.symbol;
+         eosio_assert( coinmarket_sym.is_valid(), "invalid symbol name" );
+         eosio_assert( coinmarket_amount.is_valid(), "invalid supply");
+         eosio_assert( coinmarket_amount.amount > 0, "max-supply must be positive");
+         //校验type
+         eosio_assert( type < trade_type::trade_type_count, "invalid trade type");
+         eosio_assert( ratio > 0,"invalid ratio");
+
+          tradepairs tradepair( _self,_self);
+      //    //先生成要插入表的对象
+         trade_pair trade;
+         trade.trade_id = N(coinbase_symbol + coinmarket_symbol);
+         trade.market_maker = market_maker;
+         trade.coin_base.symbol = coinbase_symbol;
+         trade.coin_base.amount = coinbase_amount;
+         trade.coin_base.coin_maker = coinbase_account;
+         trade.coin_market.symbol = coinmarket_symbol;
+         trade.coin_market.amount = coinmarket_amount;
+         trade.coin_market.coin_maker = coinmarket_account;
+
+         trade.type = type;
+
+         //插表的操作
+         tradepair.emplace(trade.trade_id, [&]( trade_pair& s ) {
+            //各种赋值的语句
+            s = trade;
+            if(s.type == trade_type::equal_ratio)
+            {
+                  equal_ratio* equal = new equal_ratio();
+                  equal->SetRatio(ratio);
+                  s.calfunc = equal;
+            }
+         });
+
 
    }
 
@@ -22,6 +66,14 @@ namespace eosio {
 
    void market_maker::exchange(int64_t trade_id,account_name account_base,account_name account_market,asset amount,coin_type type) {
 
+   }
+
+   double equal_ratio::GetBuyAmount(double amount) {
+         return amount / dRatio;
+   }
+
+   double equal_ratio::GetSellAmount(double amount) {
+         return amount * dRatio;
    }
 
 } /// namespace eosio
--- contracts/trade.market/trade.market.hpp
+++ contracts/trade.market/trade.market.hpp
@@ -39,21 +39,19 @@ namespace eosio {
       public:
          double GetBuyAmount(double amount) override;
          double GetSellAmount(double amount) override;
-         void SetRatio(double ratio);
+         void SetRatio(double ratio) {dRatio = ratio;}
       private:
          double dRatio;                         //兑换比例
    };
-
-
 
    class market_maker : public contract {
       public:
          market_maker( account_name self ):contract(self){}
          void addmarket(account_name market_maker,trade_type type,string coinbase_symbol,asset coinbase_amount,account_name coinbase_account,
-               string coinmarket_symbol,asset coinmarket_amount,account_name coinmarket_account);//新增一个交易对    
+               string coinmarket_symbol,asset coinmarket_amount,account_name coinmarket_account,double ratio);//新增一个交易对    
 
-         void addmortgage(int64_t trade_id,account_name account,asset Amount,coin_type type);//增加抵押
-         void claimmortgage(int64_t trade_id,account_name account,asset Amount,coin_type type);//取出抵押     是否需要冻结一段时间有待考虑
+         void addmortgage(int64_t trade_id,account_name account,asset amount,coin_type type);//增加抵押
+         void claimmortgage(int64_t trade_id,account_name account,asset amount,coin_type type);//取出抵押     是否需要冻结一段时间有待考虑
 
          //需要有一个查询功能  根据做市商查询这个做市商所有的交易对
          //散户使用的功能就是做交易
@@ -63,6 +61,11 @@ namespace eosio {
             string symbol;    //币种
             asset  amount;    //抵押数额
             account_name   coin_maker;//做市商账户
+            void operator=(const coin& b){
+               symbol = b.symbol;
+               amount = b.amount;
+               coin_maker = b.coin_maker;
+            }
          };
 
          struct trade_pair {  //交易对
@@ -71,9 +74,17 @@ namespace eosio {
             coin  coin_base;        //一般指共识程度较高的币种    做市上一般用这个币种来担保coin_market
             coin  coin_market;      //一般指做市上自己的币种   和coin_base属于相互担保的关系
             account_name   market_maker;// 做市商的账户     抵押中继链资源等都需要这个账户   暂时没有抵押资源这些操作
-            base_calculate calfunc;       //计算公式
+            base_calculate *calfunc;       //计算公式
 
             uint64_t primary_key()const { return trade_id; }
+
+            void operator=(const trade_pair& b){
+               trade_id = b.trade_id;
+               type = b.type;
+               coin_base = b.coin_base;
+               coin_market = b.coin_market;
+               market_maker = b.market_maker;
+            }
          };
          
          typedef eosio::multi_index<N(tradepairs), trade_pair> tradepairs;
