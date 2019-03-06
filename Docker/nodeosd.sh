030a1ee82f3d7b2f9ed93e99c62cdc364019fe71
fanyang <fanyang.coder@gmail.com>
2019-03-06 14:12:39 +0800

    use fix json in docker

--- Docker/nodeosd.sh
+++ Docker/nodeosd.sh
@@ -1,23 +1,35 @@
 #!/bin/sh
 cd /opt/FORCEIO/bin
 
-#cp /genesis.json /opt/FORCEIO/bin/data-dir
-#cp /activeacc.json /opt/FORCEIO/bin/data-dir
-cp /force.msig.wasm /opt/FORCEIO/bin/data-dir
-cp /force.msig.abi /opt/FORCEIO/bin/data-dir
-cp /force.relay.wasm /opt/FORCEIO/bin/data-dir
-cp /force.relay.abi /opt/FORCEIO/bin/data-dir
-cp /force.system.wasm /opt/FORCEIO/bin/data-dir
-cp /force.system.abi /opt/FORCEIO/bin/data-dir
-cp /force.token.wasm /opt/FORCEIO/bin/data-dir
-cp /force.token.abi /opt/FORCEIO/bin/data-dir
+cp /contracts/force.msig/force.msig.wasm /opt/FORCEIO/bin/data-dir
+cp /contracts/force.msig/force.msig.abi /opt/FORCEIO/bin/data-dir
+cp /contracts/force.relay/force.relay.wasm /opt/FORCEIO/bin/data-dir
+cp /contracts/force.relay/force.relay.abi /opt/FORCEIO/bin/data-dir
+cp /contracts/force.system/force.system.wasm /opt/FORCEIO/bin/data-dir
+cp /contracts/force.system/force.system.abi /opt/FORCEIO/bin/data-dir
+cp /contracts/force.token/force.token.wasm /opt/FORCEIO/bin/data-dir
+cp /contracts/force.token/force.token.abi /opt/FORCEIO/bin/data-dir
+mkdir /opt/FORCEIO/bin/data-dir/relay.token
+cp /contracts/relay.token/relay.token.wasm /opt/FORCEIO/bin/data-dir/relay.token
+cp /contracts/relay.token/relay.token.abi /opt/FORCEIO/bin/data-dir/relay.token
 
 if [ -f '/opt/FORCEIO/bin/data-dir/config.ini' ]; then
     echo
-  else
+else
     cp /config.ini /opt/FORCEIO/bin/data-dir
-  fi
+fi
 
+if [ -f '/opt/FORCEIO/bin/data-dir/genesis.json' ]; then
+    echo
+else
+    cp /opt/genesis.json  /opt/FORCEIO/bin/data-dir
+fi
+
+if [ -f '/opt/FORCEIO/bin/data-dir/activeacc.json' ]; then
+    echo
+else
+    cp /opt/activeacc.json /opt/FORCEIO/bin/data-dir
+fi
 
 while :; do
     case $1 in
